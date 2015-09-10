35bcf997309964056b44a9e04a6576dd78094f18
John Skilbeck <john.skilbeck@gmail.com>
2015-09-09 17:23:30 -0700

    learning Pandas

--- analysis.py
+++ analysis.py
@@ -5,8 +5,13 @@ import matplotlib.pyplot as plt
 
 als = pd.read_csv('data/analytics.csv')
 df = pd.DataFrame(data=als)
+Rent = df[df['Vertical'].isin(['Rent'])]
+RentGroupedYear = Rent.groupby(['Year']).sum()
+dff = df[df['Year'].isin([2014,2015])].groupby(['Year']).sum()
+grouped = df.groupby(['Year','Vertical']).sum()
 
-print df.groupby(['Year','Vertical']).sum()
+# print ".isin(['Rent','VRP'])"
+print RentGroupedYear
 # print df.describe()
 # print als.head()
 # print als
